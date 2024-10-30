863cd1d06794246a99dd2285c428805cf8a9c20b
hhoow0093 <howarddai2005@gmail.com>
2024-10-31 01:59:24 +0700

    blade templating engine

---
+++ 02. Blade Templating Engine/resources/views/blog.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+
+<body>
+    <a href="/">Home</a>
+    <a href="/about">about</a>
+    <a href="/blog">blog</a>
+    <a href="/contact">contact</a>
+    <h1>Judul Artikel</h1>
+    <article>
+        <h2>1. The Rise of Urban Gardening: Cultivating Green Spaces in Cities</h2>
+        <p>This article explores the growing trend of urban gardening, highlighting how communities are
+            transforming vacant lots and rooftops into vibrant green spaces. It examines the benefits for mental health,
+            food security, and biodiversity in densely populated areas.</p>
+    </article>
+    <article>
+        <h2>2. Artificial Intelligence in Healthcare: Revolutionizing Patient Care</h2>
+        <p>This article delves into the impact of artificial intelligence on the healthcare industry,
+            showcasing innovations like predictive analytics and personalized treatment plans.
+            It discusses the potential to improve patient outcomes while addressing ethical considerations and
+            challenges in
+            implementation.</p>
+    </article>
+
+</body>
+
+</html>
---
+++ 02. Blade Templating Engine/resources/views/contact.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Contact</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/about">about</a>
+    <a href="/blog">blog</a>
+    <a href="/contact">contact</a>
+    <h1>Contact</h1>
+    <ul>
+        <li><a href="https://www.youtube.com/watch?v=vDx6VA-6a6Y&list=PLFIM0718LjIW1Xb7cVj7LdAr32ATDQMdr&index=4">youtube</a></li>
+        <li><a href="https://x.com/MikaPikaZo">twitter</a></li>
+        <li><a href="https://www.instagram.com/iisma_ri/?hl=en">instagram</a></li>
+    </ul>
+    <p>email : howard@student.umn.ac.id</p>
+    <p>social media : howard_3125</p>
+</body>
+</html>
